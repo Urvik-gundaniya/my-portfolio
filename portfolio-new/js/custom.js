78b5c3af19d691f8e3af314683d6602aad789cc1
Urvik-gundaniya <urvikgundaniya@gmail.com>
2023-03-10 12:33:01 +0530

    my portfolio

---
+++ portfolio-new/js/custom.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
